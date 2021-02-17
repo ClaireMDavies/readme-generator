8e9439c115ea88b8a75b372fcd07b9c323d92d2a
Claire Davies <claire.davies@junitech.com>
2021-02-17 17:19:19 +0000

    Prelimary writing of questions

--- index.js
+++ index.js
@@ -1,7 +1,74 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+
+inquirer
+  .prompt([
+
+    /* Pass your questions in here */
+  ])
+  .then(answers => {
+    // Use user feedback for... whatever!!
+  })
+  .catch(error => {
+    if(error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else when wrong
+    }
+  });
+
+
+// An array of questions for user input
+const questions = [
+    {
+        name: "title",
+        type: "input",
+        message: "What is the title of your project?",
+    },
+    {
+        name: "project_description",
+        type: "input",
+        message: "How would you describe your project?",
+    },
+    {
+        name: "installation",
+        type: "input",
+        message: "What are the steps a user has to take to install your project?",
+    },
+    {
+        name: "usage",
+        type: "input",
+        message: "How does a user use your application?",
+    },
+    {
+        name: "usage-screenshots",
+        type: "confirm",
+        message: "Do you have any screenshots you want to include?  If yes, please upload them into an assets file in your project",
+    },
+    {
+        name: "license",
+        type: "checkbox",
+        message: "What license is used for this project?",
+        choices: ["MIT", ""]
+    },
+    {
+        name: "contributing",
+        type: "input",
+        message: "What are the Github profiles of the contributors to this project? \n If there are multiple contributors please separate the answers with a comma",
+    },
+    {
+        name: "tests",
+        type: "input",
+        message: "What is the title of your project?",
+    },
+    {
+        name: "questions",
+        type: "input",
+        message: "Write any tests you have for your app here",
+    },
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
